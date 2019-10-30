246ebc4a502c1bc72350d27a1471fdc3a6dfb328
Abdelrahman Ahmed <abdelrhman20075@gmail.com>
2019-10-30 23:41:15 +0200

    Adding elastic search

--- src/main/java/com/developerxgroup/medicinehistory/MedicineHistoryApplication.java
+++ src/main/java/com/developerxgroup/medicinehistory/MedicineHistoryApplication.java
@@ -2,7 +2,9 @@ package com.developerxgroup.medicinehistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.*;
 
+//@EnableJpaRepositories("com.developerxgroup.medicinehistory.configurations")
 @SpringBootApplication
 public class MedicineHistoryApplication {
 
--- src/main/java/com/developerxgroup/medicinehistory/controller/TestController.java
+++ src/main/java/com/developerxgroup/medicinehistory/controller/TestController.java
@@ -1,24 +1,83 @@
 package com.developerxgroup.medicinehistory.controller;
 
+import com.developerxgroup.medicinehistory.model.*;
+import org.elasticsearch.action.delete.*;
+import org.elasticsearch.action.get.*;
+import org.elasticsearch.action.index.*;
+import org.elasticsearch.action.search.*;
+import org.elasticsearch.action.update.*;
+import org.elasticsearch.client.*;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 @RestController
 @RequestMapping("/api")
 public class TestController {
-    @GetMapping("/groups")
-    List<String> groups() {
-        List<String> l = new ArrayList<>();
-        l.add("QQQQQQQQQQQQQ");
-        l.add("WWWWWWWWWWWWW");
-        l.add("EEEEEEEEEEEEE");
-        return l;
+    @Autowired
+    Client client;
+    @PostMapping("/create")
+    public String create(@RequestBody User user) throws IOException {
+        IndexResponse response = client.prepareIndex("users", "employee", user.getUserId())
+                .setSource(jsonBuilder()
+                        .startObject()
+                        .field("name", user.getName())
+                        .field("userSettings", user.getUserSettings())
+                        .endObject()
+                )
+                .get();
+        System.out.println("response id:"+response.getId());
+        return response.getResult().toString();
     }
-
+    @GetMapping("/view/{id}")
+    public Map<String, Object> view(@PathVariable final String id) {
+        GetResponse getResponse = client.prepareGet("users", "employee", id).get();
+        return getResponse.getSource();
+    }
+    @GetMapping("/view/name/{field}")
+    public Map<String, Object> searchByName(@PathVariable final String field) {
+        Map<String,Object> map = null;
+        SearchResponse response = client.prepareSearch("users")
+                .setTypes("employee")
+                .setSearchType(SearchType.QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.matchQuery("name", field))
+                                .get()
+        ;
+        List<SearchHit> searchHits = Arrays.asList(response.getHits().getHits());
+        map =   searchHits.get(0).getSourceAsMap();
+        return map;
+    }
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable final String id) throws IOException {
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.index("users")
+                .type("employee")
+                .id(id)
+                .doc(jsonBuilder()
+                        .startObject()
+                        .field("name", "Rajesh")
+                        .endObject());
+        try {
+            UpdateResponse updateResponse = client.update(updateRequest).get();
+            System.out.println(updateResponse.status());
+            return updateResponse.status().toString();
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println(e);
+        }
+        return "Exception";
+    }
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable final String id) {
+        DeleteResponse deleteResponse = client.prepareDelete("users", "employee", id).get();
+        return deleteResponse.getResult().toString();
+    }
 }
